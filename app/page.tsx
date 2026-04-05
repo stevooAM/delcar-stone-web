ef32960b827e5ea12e199d5b6787520a67ab2de0
Steve Adjei-Mensah <61298634+stevooAM@users.noreply.github.com>
2026-04-05 12:35:27 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,6 @@ export default function Home() {
         <FeaturedSection />
         <WhySection />
         <ApplicationsSection />
-        <CatalogueStrip />
         <CTASection />
         <ContactSection />
       </main>
